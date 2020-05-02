cf04acb6ecfdcfdee8807b8ee98878d56278450a
Blackdevil122 <akshat.ag@somaiya.edu>
2020-05-02 18:22:22 +0530

    database updated with global matplotib

--- analysis.py
+++ analysis.py
@@ -10,15 +10,51 @@ sum_amount=0
 sum_total=0
 db = sqlite3.connect('myspendmate.db')
 cursor = db.cursor()
-month_name=['jan','feb','mar','apr','may']
-lis=[]
-expens=[]
 cursor.execute("select sum(amount) from incomeee")
 sum1 = cursor.fetchone()[0]
 if(sum1=='None'):
     sum1=str(0)
 sum_total = sum1
 print("sum:"+str(sum_total))
+cate_cont=[]
+cate_cont_per=[]
+count=-1
+cate_list=[]
+cursor.execute("SELECT * FROM incomeee")
+list12=cursor.fetchall()
+for j in list12:
+    print(j[3])
+    category=str(j[3])
+    amtt=int(j[0])
+    if cate_list.count(category)==0:
+        cate_list.append(category)
+        count=count+1
+        sum_cate=amtt
+        cate_cont.append(sum_cate)
+        sum_cate= float((sum_cate/sum_total)*100)
+        cate_cont_per.append(sum_cate)
+        
+    else:
+        cate_cont[count]= cate_cont[count]+amtt
+        sum_cate = cate_cont[count]
+        sum_cate= float((sum_cate/sum_total)*100)
+        cate_cont_per[count]=sum_cate
+
+
+print(cate_list)
+print(cate_cont)
+print(cate_cont_per)
+
+    
+
+    
+    
+
+
+month_name=['jan','feb','mar','apr','may']
+lis=[]
+expens=[]
+
 for i in range(1,5):
     cursor.execute("select sum(amount) from incomeee where month= '%d'"%i)
     sum_mon=cursor.fetchone()[0]
@@ -64,10 +100,11 @@ def callAnalysis(root):
 
     def printpie():
         # Data to plot
-        labels = lis
-        sizes = expens
-        colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
-        explode = (0.1, 0, 0, 0)  # explode 1st slice
+        
+        labels = cate_list
+        sizes = cate_cont_per
+        colors = ['gold']
+        explode = (0)  # explode 1st slice
 
         # Plot
         plt.pie(sizes, explode=explode, labels=labels, colors=colors,
